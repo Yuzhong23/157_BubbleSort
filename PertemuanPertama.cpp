01a00403f57be7493f509549960401108aec433e
Yuzhong23 <xixixixifatur@gmail.com>
2024-03-07 12:18:13 +0700

    membuat Display

--- PertemuanPertama.cpp
+++ PertemuanPertama.cpp
@@ -34,10 +34,9 @@ void display() {                      //prosedure untuk menampilakn hasil
 	cout << "Elemen Array yang telah tersusun" << endl;    //output ke layar
 	cout << "================================" << endl;    //output ke layar
 	for (int j = 0; j < n; j++) {                 //looping dengan j di mulai dari 0 hingga n-1
-		cout << "Data ke-" << j + 1 << ";";
+		cout << "Data ke-" << j + 1 << ": ";
 		cout << a[j] << endl;                    //output ke layar
 	} 
 	cout << endl;                               //output baris kosong
 }
-
 
